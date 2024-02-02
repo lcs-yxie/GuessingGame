3ee8f7d18bd0f9a917c3324d62e6e14d1ec1e4b4
Yukun Xie <yxie@lcs.on.ca>
2024-02-02 08:53:19 -0500

    Changed to gaurd statement

--- GuessingGame/GameView.swift
+++ GuessingGame/GameView.swift
@@ -13,7 +13,7 @@ struct GameView: View {
     // MARK: Stored properties
     
     // What number the user has guessed
-    @State var selectedNumber = 50
+    @State var givenInput = ""
     
     // What number the computer wants the user to guess
     @State var target = Int.random(in: 1...100)
@@ -35,13 +35,7 @@ struct GameView: View {
                 Text("Guess what it is!")
                     .font(.headline)
                 
-                Stepper(value: $selectedNumber, in: 1...100) {
-                    HStack {
-                        Text("Make a guess:")
-                        Spacer()
-                        Text("\(selectedNumber)")
-                    }
-                }
+                TextField("Make a guess", text: $givenInput)
                 
                 Button {
                     checkGuess()
@@ -94,6 +88,11 @@ struct GameView: View {
     // MARK: Functions
     func checkGuess() {
         
+        guard let selectedNumber = Int(givenInput) else {
+            feedback = "Please provide an integer."
+            return
+        }
+        
         // Provide feedback to the user
         // When should they guess higher?
         // When should then guess lower?
@@ -117,7 +116,7 @@ struct GameView: View {
     func reset() {
 
         // Start the user back at 50
-        selectedNumber = 50
+        givenInput = ""
         
         // Have the computer guess a new number
         target = Int.random(in: 1...100)
